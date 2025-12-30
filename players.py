dce5c8124fa64c93bd841f19895384a9af297be3
bartlomiej.hryniewski@gmail.com <bartlomiej.hryniewski@gmail.com>
2025-12-30 13:57:27 +0100

    added template files for .env, players.py, and match_data.csv

---
+++ players.py
@@ -0,0 +1,40 @@
+# list of players organised by rank
+
+bronze = [
+    "player1#TAG1", "player2#TAG2", "player3#TAG3"
+]
+
+silver = [
+    "player1#TAG1", "player2#TAG2", "player3#TAG3"
+]
+
+gold = [
+    "player1#TAG1", "player2#TAG2", "player3#TAG3"
+]
+
+platinum = [
+    "player1#TAG1", "player2#TAG2", "player3#TAG3"
+]
+
+emerald = [
+    "player1#TAG1", "player2#TAG2", "player3#TAG3"
+]
+
+diamond = [
+    "player1#TAG1", "player2#TAG2", "player3#TAG3"
+]
+
+master = [
+    "player1#TAG1", "player2#TAG2", "player3#TAG3"
+]
+
+grandmaster = [
+    "player1#TAG1", "player2#TAG2", "player3#TAG3"
+]
+
+challenger = [
+    "player1#TAG1", "player2#TAG2", "player3#TAG3"
+]
+
+# combine all players
+all_players = bronze + silver + gold + platinum + emerald + diamond + master + grandmaster + challenger
